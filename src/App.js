3711fd7b61fbdf511850cc96485320069e87c7c0
Nathan Trippe <nathan.trippe@warnermedia.com>
2024-01-26 15:39:10 -0500

    Set up first working attempt at FAVE Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import LiveStream from './components/livestream';
-import FavePlayer from './components/fave-player';
+import "./App.css";
+import FavePlayer from "./components/fave-player";
 
-const liveStreamAddress = 'https://fave.api.cnn.io/v1/demo/?mediumId=livec76319f599742ab668c8b3ba6dcfed3ce7e817ad&mediumEnvironment=prod&isLive=true';
+const liveStreamAddress =
+  "https://fave.api.cnn.io/v1/demo/?mediumId=livec76319f599742ab668c8b3ba6dcfed3ce7e817ad&mediumEnvironment=prod&isLive=true";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="AnotherContainerTest">
+        <FavePlayer></FavePlayer>
+      </div>
     </div>
   );
 }
