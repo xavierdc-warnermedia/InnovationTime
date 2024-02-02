2e115c0414099833d9ed802dc659f1889a3f3407
xavierdc-warnermedia <133021786+xavierdc-warnermedia@users.noreply.github.com>
2024-02-02 17:01:55 -0500

    added aws dynamo package, added some css, added notes for ideas for myself

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import FavePlayer from "./components/fave-player";
 // const liveStreamAddress =
   // "https://fave.api.cnn.io/v1/demo/?mediumId=livec76319f599742ab668c8b3ba6dcfed3ce7e817ad&mediumEnvironment=prod&isLive=true";
 
+const nothing = () => {
+  alert('nothing happened!');
+}
+
 function App() {
   return (
     <div className="App">
       <div className="AnotherContainerTest">
         <FavePlayer />
+        <button className="button" onClick={nothing}>This button doesn't do anything</button>
       </div>
     </div>
   );
