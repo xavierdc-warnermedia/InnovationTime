5329104e2f07d8c3013ae199a57d145f53f85941
xavierdc-warnermedia <xavier.decardenas@wbd.com>
2024-03-01 16:45:48 -0500

    moved stuff around, added flexbox for side container, added css

--- src/App.js
+++ src/App.js
@@ -5,17 +5,12 @@ import DynamicContainer from "./components/dynamic-container";
 // const liveStreamAddress =
   // "https://fave.api.cnn.io/v1/demo/?mediumId=livec76319f599742ab668c8b3ba6dcfed3ce7e817ad&mediumEnvironment=prod&isLive=true";
 
-const nothing = () => {
-  alert('nothing happened!');
-}
-
 function App() {
   return (
     <div className="App">
       <div className="AnotherContainerTest">
         <FavePlayer />
         <DynamicContainer />
-        <button className="button" onClick={nothing}>This button doesn't do anything</button>
       </div>
     </div>
   );
--- src/components/dynamic-container/DynamicContainer.js
+++ src/components/dynamic-container/DynamicContainer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './DynamicContainer.css';
 
 const DynamicContainer = () => {
+  const nothing = () => {
+    alert('nothing happened');
+  }
+
   return (
-    <div>
+    <div className="container">
       <h3>DynamicContainer</h3>
+      <button className="button" onClick={nothing}>This button doesn't do anything</button>
     </div>
   )
 }
